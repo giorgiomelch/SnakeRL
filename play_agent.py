d9f8e26103d0b8bf146cdc35a14b277933bd7ca0
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2024-10-15 19:08:51 +0200

    first commit

---
+++ play_agent.py
@@ -0,0 +1,74 @@
+import argparse
+import enviroment_visual as enviroment_visual
+import numpy as np
+import keras
+
+def state_to_index(state_vector):
+    pericoli = state_vector[0] * 4 + state_vector[1] * 2 + state_vector[2]
+    direzione_corrente = state_vector[3] * 3 + state_vector[4] * 2 + state_vector[5] * 1
+    posizione_frutto = np.mod(state_vector[7] * 6 + state_vector[8] * 3 + state_vector[9] * 2 + state_vector[10], 8)
+    return pericoli * 32 + direzione_corrente * 8 + posizione_frutto
+def initialize_QTable():
+    return np.zeros((256, 3))
+
+
+def tab_agent(speed=10):
+    file = 'Q_table/ExplorationFunction/1000000step.npy' 
+    Q_table = np.load(file)
+    env_visual = enviroment_visual.SnakeGameAI(w=400, h=400, speed=speed)
+    env_visual.reset()
+    game_over = False
+    while not game_over:
+        state= env_visual.get_state()
+        row_Q_value = state_to_index(state)
+        action = np.argmax(Q_table[row_Q_value])
+        final_move = [0,0,0]
+        final_move[action] = 1
+        _, _, game_over, score = env_visual.play_step(final_move)
+        
+    env_visual.quit()
+    print(f"Score: {score}\n", end="")
+
+def lnn_agent(speed=10):
+    file = "DQNmodel/LNN/model.keras"
+    env_visual = enviroment_visual.SnakeGameAI(w=400, h=400, speed=speed)
+    model = keras.models.load_model(file)
+    game_over = False
+    state = env_visual.get_state()
+    while not game_over:
+        action = np.argmax(model(state[np.newaxis])[0])
+        final_move = [0,0,0]
+        final_move[action] = 1
+        state, _, game_over, score = env_visual.play_step(final_move)
+    env_visual.quit()
+    print(f"Score: {score}\n", end="")
+
+def cnn_agent(speed):
+    file = "DQNmodel/CNN/model.keras"
+    env_visual = enviroment_visual.SnakeGameAI(w=400, h=400, speed=speed)
+    model = keras.models.load_model(file)
+    game_over = False
+    state = env_visual.get_matrix_state()
+    while not game_over:
+        action = np.argmax(model(state[np.newaxis])[0])
+        final_move = [0,0,0,0]
+        final_move[action] = 1
+        state, _, game_over, score = env_visual.play_step_m(final_move)
+    env_visual.quit()
+    print(f"Score: {score}\n", end="")
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Choose which Snake agent to run.")
+    parser.add_argument('agent', choices=['tab', 'lnn', 'cnn'], help="Choose the agent: 'tab', 'lnn', or 'cnn'")
+    parser.add_argument('--speed', type=int, help="Speed of the Snake game", default=10)
+
+    args = parser.parse_args()
+    
+    if args.agent == 'tab':
+        tab_agent(speed=args.speed)
+    elif args.agent == 'lnn':
+        lnn_agent(speed=args.speed)
+    elif args.agent == 'cnn':
+        cnn_agent(speed=args.speed)
