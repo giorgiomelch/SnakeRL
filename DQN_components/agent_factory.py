8d1ec21ba1c1ea48285fb9476525b7decf7cf5f0
giorgiomelch <122287332+giorgiomelch@users.noreply.github.com>
2024-10-17 13:28:39 +0200

    scelta file path delegata alla train function

--- DQN_components/agent_factory.py
+++ DQN_components/agent_factory.py
@@ -14,15 +14,15 @@ def convert_to_tensorflow(states, actions, rewards, next_states, dones):
         
 class Agent:
     def __init__(self, enviroment, lr, gamma, max_memory, batch_size, input_shape, n_actions, model_units=[256], 
-                 eps_greedy=True, file_name_model="model.keras"):
+                 eps_greedy=True):
         self.n_games = 0
         self.epsilon = 1
         self.batch_size = batch_size
         self.memory = replay_buffer.CircularBuffer(max_size=max_memory)
         self.dqnetwork = model_factory.QNetwork(lr=lr, gamma=gamma, input_shape=input_shape, n_output=n_actions, units=model_units)
         self.env = enviroment
+        self.n_actions = n_actions
         self.exploration_policy = self.epsilon_greedy_policy if eps_greedy else self.softmax_policy
-        self.file_name_model = file_name_model
 
     def remember(self, state, action, reward, next_state, done):
         self.memory.append(convert_to_tensorflow(state, action, reward, next_state, done))
@@ -33,33 +33,32 @@ class Agent:
 
     def softmax_policy(self, state):
         Q_values = self.dqnetwork.online_model(state[np.newaxis])
-        if self.epsilon < 0.005: # per evitare buffer overflow
+        if self.epsilon < 0.005: # soglia inserita per evitare buffer overflow
             return np.argmax(Q_values[0])
         else:
             max_Q = np.max(Q_values)
             exp_q = np.exp((Q_values - max_Q) / self.epsilon)
             probabilities = exp_q / np.sum(exp_q)
-            action = np.random.choice(3, p=probabilities[0])
+            action = np.random.choice(self.n_actions, p=probabilities[0])
             return action
         
     def epsilon_greedy_policy(self, state):
         if np.random.rand() < self.epsilon:
-            return np.random.randint(3)
+            return np.random.randint(self.n_actions)
         else:
             Q_values = self.dqnetwork.model(state[np.newaxis])
             return np.argmax(Q_values[0])
         
     def get_action(self, state):
-        final_move = [0, 0, 0]  
+        final_move = np.zeros(self.n_actions) # array di zero per codifica onw hot delle azioni
         move = self.exploration_policy(state)
         final_move[move] = 1
         return final_move
     
-    def train_agent(self, N_GAME):
+    def train_agent(self, N_GAME, episode_decay, directory_path="", file_name_model=""):
         score_list = []
         record = 0
         step=0
-        n_eps_zero = int(N_GAME*0.7)
         while self.n_games < N_GAME:
             state_old = self.env.get_state()
             final_move = self.get_action(state_old)
@@ -70,10 +69,10 @@ class Agent:
                 self.n_games += 1
                 self.train_memory()
                 print(f"\rGame: {self.n_games}, Epsilon: {self.epsilon:3f}, Score: {score}, Record: {record}, Step eseguiti: {step}. ", end="")
-                self.epsilon = max(((n_eps_zero - self.n_games) / n_eps_zero), 0)
+                self.epsilon = max(((episode_decay - self.n_games) / episode_decay), 0)
                 if score > record:
                     record = score
-                    self.dqnetwork.save_model(file_name=self.file_name_model)
+                    self.dqnetwork.save_model(directory_path, file_name_model)
                 score_list.append(score)
             step+=1
 
@@ -84,16 +83,15 @@ class Agent:
 
 class Agent_DoubleDQN(Agent):
     def __init__(self, enviroment, lr, gamma, max_memory, batch_size, input_shape, n_actions, model_units=[256], 
-                 eps_greedy=True, file_name_model="model_DOUBLE.keras"):
+                 eps_greedy=True):
         super().__init__(enviroment, lr, gamma, max_memory, batch_size, input_shape, n_actions, model_units, 
-                         eps_greedy, file_name_model)
-        self.dqnetwork = model_factory.DoubleQNetwork(lr=lr, gamma=gamma, units=model_units)
+                         eps_greedy)
+        self.dqnetwork = model_factory.DoubleQNetwork(lr=lr, gamma=gamma, input_shape=input_shape, n_output=n_actions, units=model_units)
 
-    def train_agent(self, N_GAME):
+    def train_agent(self, N_GAME, episode_decay, update_target_model=5, directory_path="", file_name_model=""):
         score_list = []
         record = 0
         step=0
-        n_eps_zero = int(N_GAME*0.7)
         while self.n_games < N_GAME:
             state_old = self.env.get_state()
             final_move = self.get_action(state_old)
@@ -104,11 +102,11 @@ class Agent_DoubleDQN(Agent):
                 self.n_games += 1
                 self.train_memory()
                 print(f"\rGame: {self.n_games}, Epsilon: {self.epsilon:3f}, Score: {score}, Record: {record}, Step eseguiti: {step}. ", end="")
-                self.epsilon = max(((n_eps_zero - self.n_games) / n_eps_zero), 0)
+                self.epsilon = max(((episode_decay - self.n_games) / episode_decay), 0)
                 if score > record:
                     record = score
-                    self.dqnetwork.save_model()
-                if self.n_games % 5:
+                    self.dqnetwork.save_model(directory_path, file_name_model)
+                if self.n_games % update_target_model:
                     self.dqnetwork.update_weights()
                 score_list.append(score)
             step+=1
@@ -118,11 +116,11 @@ class Agent_DoubleDQN(Agent):
 
 class Agent_PER(Agent):
     def __init__(self, enviroment, lr, gamma, max_memory, batch_size, input_shape, n_actions, model_units=[256], 
-                 eps_greedy=True, file_name_model="model_PER.keras"):
+                 eps_greedy=True):
         super().__init__(enviroment, lr, gamma, max_memory, batch_size, input_shape, n_actions, model_units, 
-                         eps_greedy, file_name_model)
+                         eps_greedy)
         self.memory = replay_buffer.PrioritizedReplayBuffer(max_size=max_memory, zeta=0.6)
-        self.dqnetwork = model_factory.PER_QNetwork(lr=lr, gamma=gamma, units=model_units)
+        self.dqnetwork = model_factory.PER_QNetwork(lr=lr, gamma=gamma,input_shape= input_shape, n_output=n_actions, units=model_units)
 
     def train_memory(self, beta=0.4):
         if len(self.memory.buffer) < self.batch_size:
@@ -134,11 +132,11 @@ class Agent_PER(Agent):
 
 class Agent_DDQN_PER(Agent_DoubleDQN):
     def __init__(self, enviroment, lr, gamma, max_memory, batch_size, input_shape, n_actions, model_units=[256], 
-                 eps_greedy=True, file_name_model="model_DDQN_PER.keras"):
+                 eps_greedy=True):
         super().__init__(enviroment, lr, gamma, max_memory, batch_size, input_shape, n_actions, model_units, 
-                         eps_greedy, file_name_model)
+                         eps_greedy)
         self.memory = replay_buffer.PrioritizedReplayBuffer(max_size=max_memory, zeta=0.6)
-        self.dqnetwork = model_factory.DDQN_PER_QNetwork(lr=lr, gamma=gamma, units=model_units)
+        self.dqnetwork = model_factory.DDQN_PER_QNetwork(lr=lr, gamma=gamma,input_shape= input_shape, n_output=n_actions, units=model_units)
 
     def train_memory(self, beta=0.4):
         if len(self.memory.buffer) < self.batch_size:
